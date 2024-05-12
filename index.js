49d5cbc4ab2964b56a77ae2914b8aa189dc30e5d
Emie-Trialer <emiedonmukumo@gmail.com>
2024-05-12 07:09:33 +0100

    Added Cors to External Request

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const blog = require('./controller/blog')
 require('dotenv').config()
 const bodyParser = require('body-parser')
 
+const cors = require('cors')
+app.use(cors())
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
